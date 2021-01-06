8aeac0a4c8c3e87d8f87801d7dd426b986c6635a
Sebastien La Duca <sladuca777@gmail.com>
2021-01-06 09:52:09 -0500

    point to Sladuca llvm forks

--- osx/build.sh
+++ osx/build.sh
@@ -7,16 +7,16 @@ rm -rf out
 mkdir -p out
 cd out
 
-git clone https://github.com/solana-labs/llvm.git
-echo "$( cd llvm && git rev-parse HEAD )  https://github.com/solana-labs/llvm.git" >> version.md
-git clone https://github.com/solana-labs/clang.git llvm/tools/clang
-echo "$( cd llvm/tools/clang && git rev-parse HEAD )  https://github.com/solana-labs/clang.git" >> version.md
-git clone https://github.com/solana-labs/clang-tools-extra.git llvm/tools/clang/tools/extra
-echo "$( cd llvm/tools/clang/tools/extra && git rev-parse HEAD )  https://github.com/solana-labs/clang-tools-extra.git" >> version.md
-git clone https://github.com/solana-labs/compiler-rt.git llvm/projects/compiler-rt
-echo "$( cd llvm/projects/compiler-rt && git rev-parse HEAD )  https://github.com/solana-labs/compiler-rt.git" >> version.md
-git clone https://github.com/solana-labs/lld.git llvm/tools/lld
-echo "$( cd llvm/tools/lld && git rev-parse HEAD )  https://github.com/solana-labs/lld.git" >> version.md
+git clone https://github.com/Sladuca/llvm.git
+echo "$( cd llvm && git rev-parse HEAD )  https://github.com/Sladuca/llvm.git" >> version.md
+git clone https://github.com/Sladuca/clang.git llvm/tools/clang
+echo "$( cd llvm/tools/clang && git rev-parse HEAD )  https://github.com/Sladuca/clang.git" >> version.md
+git clone https://github.com/Sladuca/clang-tools-extra.git llvm/tools/clang/tools/extra
+echo "$( cd llvm/tools/clang/tools/extra && git rev-parse HEAD )  https://github.com/Sladuca/clang-tools-extra.git" >> version.md
+git clone https://github.com/Sladuca/compiler-rt.git llvm/projects/compiler-rt
+echo "$( cd llvm/projects/compiler-rt && git rev-parse HEAD )  https://github.com/Sladuca/compiler-rt.git" >> version.md
+git clone https://github.com/Sladuca/lld.git llvm/tools/lld
+echo "$( cd llvm/tools/lld && git rev-parse HEAD )  https://github.com/Sladuca/lld.git" >> version.md
 
 mkdir -p llvm/build
 pushd llvm/build
